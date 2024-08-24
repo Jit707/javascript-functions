27df5ed3cbbccef58f8f0b398d325f3749a71807
Jit <jit.oung254343@gmail.com>
2024-08-25 00:07:21 +0700

    add finished assignment

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,19 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+    add : function add(a,b){
+        return a+b
+    },
+    substract : function(a,b){
+        return a-b
+    },
+    multiply : function(a,b){
+        return a*b
+    },
+    divide : function(a,b){
+        return a/b
+    },
+};
+let addResult = (calculator.add(10,20))
+console.log(addResult);
+let devideResult = (calculator.divide(3000,10))
+console.log(devideResult);
